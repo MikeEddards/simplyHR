306aa466c352eaad61b15c518ed99b78ecb6fa93
BigMike783 <45283126+BigMike783@users.noreply.github.com>
2019-08-20 19:54:17 -0600

    added static serve

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express()
 
 
 app.use(express.json())
-// app.use( express.static( `${__dirname}/../build` ) );
+app.use( express.static( `${__dirname}/../build` ) );
 
 const db = mysql.createPool({
     host: DB_HOST,
